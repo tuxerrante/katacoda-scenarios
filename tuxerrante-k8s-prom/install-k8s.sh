5a5620b5b9c1ba49c96e2866393d9b302ff07763
Alessandro Affinito <alessandro.affinito@ptvgroup.com>
2021-11-11 11:30:47 +0100

    switched to k8s image due to low memory

--- tuxerrante-k8s-prom/install-k8s.sh
+++ tuxerrante-k8s-prom/install-k8s.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 apt update
-apt install kubeadm=1.19.4-00 && kubeadm config images pull --kubernetes-version 1.19.4 && kubeadm upgrade apply v1.19.4 --yes 
+apt install -ykubeadm=1.19.4-00 && \
+	kubeadm config images pull --kubernetes-version 1.19.4 && \
+	kubeadm upgrade apply v1.19.4 --yes 
 apt install -y --allow-change-held-packages kubeadm=1.19.4-00 > /dev/null 2>&1
 ssh node01 'apt install kubeadm=1.19.4-00 && kubeadm upgrade node'
 kubectl -n kube-system get cm kubeadm-config -oyaml
