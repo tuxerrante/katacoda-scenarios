95dd44f9e79e3ebec513e82723dbc584aae23447
Alessandro Affinito <alessandro.affinito@ptvgroup.com>
2021-11-11 13:09:54 +0100

    install 1.22 from scratch

--- tuxerrante-k8s-prom/install-k8s.sh
+++ tuxerrante-k8s-prom/install-k8s.sh
@@ -1,13 +1,16 @@
 #/bin/bash
-kubeadm reset -y
+kubeadm reset
+
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-printf "=====\n"
+echo -n "\ndeb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+echo -n "\n====="
 apt-get update >/dev/null 2>&1
 apt-cache policy kubeadm |head
 apt-get install -y kubelet=1.22.3-00 kubeadm=1.22.3-00 kubectl=1.22.3-00
+
 apt-mark hold kubelet kubeadm kubectl
-printf "=====\n"
+echo -n "\n====="
 kubeadm init --pod-network-cidr=192.168.0.0/16
 export TOKEN=$(kubeadm token list -o=jsonpath="{.token}")
 export TOKEN_HASH=$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')
@@ -18,9 +21,9 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 export KUBECONFIG=/etc/kubernetes/admin.conf
 curl https://docs.projectcalico.org/manifests/calico.yaml -O
 kubectl apply -f calico.yaml  >/dev/null
-printf "=====> Waiting for cluster to start \n"
+echo -n "\n=====> Waiting for cluster to start "
 launch.sh 
 kubectl get pods --all-namespaces
-printf "=====> Updating worker node \n"
+echo -n "\n=====> Updating worker node "
 # kubectl -n kube-system get cm kubeadm-config -oyaml
 ./install-prom.sh
