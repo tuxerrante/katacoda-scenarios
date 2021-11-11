663c595cc72e530032fc567f72183f8404ae8bd6
Alessandro Affinito <alessandro.affinito@ptvgroup.com>
2021-11-11 11:52:30 +0100

    fix autoupgrade

--- tuxerrante-k8s-prom/install-k8s.sh
+++ tuxerrante-k8s-prom/install-k8s.sh
@@ -1,11 +1,14 @@
 #/bin/bash
 apt update
-apt install -y kubeadm=1.19.4-00 && \
+apt install -y kubeadm=1.19.4-00 kubectl=1.19.4-00 && \
 	kubeadm config images pull --kubernetes-version 1.19.4
+echo "=====> Waiting for cluster to start"
 launch.sh 
+echo "=====> Upgrading cluster to 1.19.4 "
 kubeadm upgrade apply v1.19.4 --yes
 # apt install -y --allow-change-held-packages kubeadm=1.19.4-00 > /dev/null 2>&1
-ssh node01 'apt update; apt install -y kubelet=1.19.3-00 && kubeadm upgrade node'
+echo "=====> Updatng worker node"
+ssh node01 'apt update; apt install -y kubelet=1.19.3-00'
 # kubectl -n kube-system get cm kubeadm-config -oyaml
 apt install -y kubelet=1.19.4-00 kubectl=1.19.4-00 > /dev/null
 kubeadm upgrade apply v1.20.12 --force
