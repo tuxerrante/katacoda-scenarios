60a39ed034f6f4848b0868989286ef84cacb3870
Alessandro Affinito <alessandro.affinito@ptvgroup.com>
2021-11-11 11:42:08 +0100

    fixed kubelet install

--- tuxerrante-k8s-prom/install-k8s.sh
+++ tuxerrante-k8s-prom/install-k8s.sh
@@ -4,7 +4,8 @@ apt install -y kubeadm=1.19.4-00 && \
 	kubeadm config images pull --kubernetes-version 1.19.4 && \
 	kubeadm upgrade apply v1.19.4 --yes 
 apt install -y --allow-change-held-packages kubeadm=1.19.4-00 > /dev/null 2>&1
-ssh node01 'apt install kubeadm=1.19.4-00 && kubeadm upgrade node'
+ssh node01 'apt update; apt install -y kubelet=1.19.3-00 && kubeadm upgrade node'
 # kubectl -n kube-system get cm kubeadm-config -oyaml
-apt install -y kubelet=1.19.4-00 kubectl=1.19.4-00 > /dev/null 2>&
+apt install -y kubelet=1.19.4-00 kubectl=1.19.4-00 > /dev/null
+kubeadm upgrade apply v1.20.12 --force
 ./launch.sh 
