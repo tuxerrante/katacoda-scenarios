f7e01c3ef6239f5a231b83d21d9da32e513a048b
Alessandro Affinito <alessandro.affinito@ptvgroup.com>
2021-11-11 13:02:41 +0100

    install 1.22 from scratch

--- tuxerrante-k8s-prom/install-k8s.sh
+++ tuxerrante-k8s-prom/install-k8s.sh
@@ -1,15 +1,37 @@
 #/bin/bash
-apt update >/dev/null 2>&1
-apt install -y kubeadm=1.19.4-00 kubectl=1.19.4-00 && \
-	kubeadm config images pull --kubernetes-version 1.19.4 >/dev/null 2>&1
-echo "=====> Waiting for cluster to start"
+
+kubeadm reset -y
+
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update >/dev/null 2>&1
+
+apt-cache policy kubeadm |head
+apt-get install -y kubelet=1.22.3-00 kubeadm=1.22.3-00 kubectl=1.22.3-00
+apt-mark hold kubelet kubeadm kubectl
+
+kubeadm init --pod-network-cidr=192.168.0.0/16
+
+export TOKEN=$(kubeadm token list -o=jsonpath="{.token}")
+export TOKEN_HASH=$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //')
+ssh node01 "apt update; apt install -y kubelet=1.22.3-00 kubeadm=1.22.3-00; kubeadm join --token ${TOKEN } controlplane:6443 --discovery-token-ca-cert-hash sha256:${TOKEN_HASH}" &
+
+mkdir -p $HOME/.kube
+cp /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+curl https://docs.projectcalico.org/manifests/calico.yaml -O
+kubectl apply -f calico.yaml  >/dev/null
+
+
+
+printf "=====> Waiting for cluster to start \n"
 launch.sh 
-echo "=====> Upgrading cluster to 1.19.4 "
-kubeadm upgrade apply v1.19.4 --yes
-# apt install -y --allow-change-held-packages kubeadm=1.19.4-00 > /dev/null 2>&1
-echo "=====> Updatng worker node"
-ssh node01 'apt update; apt install -y kubelet=1.19.3-00'
+kubectl get pods --all-namespaces
+printf "=====> Updating worker node \n"
+
 # kubectl -n kube-system get cm kubeadm-config -oyaml
-apt install -y kubelet=1.19.4-00 kubectl=1.19.4-00 > /dev/null
-kubeadm upgrade apply v1.20.12 --force
+
 ./install-prom.sh
